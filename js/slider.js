d0d335ccb2abe6e106e3ea4e0c703c427e041e8e
Skaper777 <skaper777@gmail.com>
2019-02-07 21:04:17 +0300

    slider

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,8 @@
     var slider = new Swiper('.slider', {
         //loop: true,
         //loopedSlides: 5,
+        effect: 'fade',
+        speed: 1100,
         autoplay: {
             delay: 2500,
             disableOnInteraction: false,
